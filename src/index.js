8334550a9a39cf4cbcb08a968f280f72c421dbda
John Devitt <johndevittire@gmail.com>
2018-08-11 10:00:02 +0200

    add react router and routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 /* eslint-env browser */
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'grommet/components/App'
+import 'grommet/scss/vanilla/index.scss'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import QuestionListContainer from './containerComponents/QuestionListContainer'
+import QuestionContainer from './containerComponents/QuestionContainer'
 
-const Index = () => <div>Hello React!</div>;
+const Index = () => (
+	<Router>
+		<App centered={false}>
+			<Route exact path="/" component={QuestionListContainer} />
+			<Route exact path="/questions/:id" component={QuestionContainer} />
+		</App>
+	</Router>
+)
 
-ReactDOM.render(<Index />, document.getElementById('index'));
+ReactDOM.render(<Index />, document.getElementById('index'))
